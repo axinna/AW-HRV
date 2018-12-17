91e8fdf76d1205d9634e4e282fbb2c6bcd31eda2
axin <527362824@qq.com>
2018-12-17 22:12:46 +0800

    2
    2

---
+++ 201812测试/数据预处理ing/pretest.m
@@ -0,0 +1,23 @@
+% hrData_raw = textread('C:\Users\Administrator\Desktop\20181216²âÊÔ\test.txt','%n');
+% load raw1812162220.mat
+var = raw; 
+len = length(raw);
+
+for i=len:-1:1
+    if raw(i) >= 1.2
+        var(i) = [];
+    end
+end
+
+len= length(var);
+changeflag=0;
+j=0;
+for i=1:len-1
+    if var(i) ~= var(i+1)
+        j=j+1;
+        result(j,1)=var(i);
+    end
+end
+
+
+
